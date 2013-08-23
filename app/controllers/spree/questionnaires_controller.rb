f69663c8b589e635f807f6d38f8eca36329041de
Numerico <webmaster@numerica.cl>
2013-08-23 12:58:49 -0400

    questionnaire first link

--- app/controllers/spree/questionnaires_controller.rb
+++ app/controllers/spree/questionnaires_controller.rb
@@ -2,6 +2,7 @@ class Spree::QuestionnairesController < Spree::StoreController
 
   def show
     @questionnaire = Questionnaire.get_questionnaire
+    @question_one = @questionnaire.ordered_questions.first unless @questionnaire.nil?
   end
 
 end
--- spec/views/spree/questionnaire/show.html.erb_spec.rb
+++ spec/views/spree/questionnaire/show.html.erb_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe "questionnaire/show.html.erb" do
 
-    it "displays an introduction" do
-      create :questionnaire
-      visit spree.questionnaire_path
-      within "#wrapper" do
-        page.should have_selector 'p'
-      end
+  it "displays an introduction" do
+    create :questionnaire
+    visit spree.questionnaire_path
+    within "#wrapper" do
+      page.should have_selector 'p'
     end
+  end
+
+  it "has a link to the first question" do
+    questionnaire = create :questionnaire_with_questions
+    question_one = questionnaire.ordered_questions.first
+    visit spree.questionnaire_path
+    within "#wrapper" do
+      page.should have_selector "a[href='"+spree.questionnaire_question_path(question_one)+"']"
+    end
+  end
 
 end
